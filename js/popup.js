7c2224a9b09a42e044b87a8f4aa53a11a09a6d56
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2015-12-26 21:47:05 +0700

    add content to setting tab

--- js/popup.js
+++ js/popup.js
@@ -11,19 +11,66 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 	    var res = request.message.content;
 	    if(res){
 	    	for(var i in res){
-	    		trBody += '<tr>'
+	    		trBody += '<tr id="tr'+i+'">'
 	    			+'<td class="link">'+res[i].link+'</td>'
 	    			+'<td class="status">'+res[i].status+'</td>'
 	    			+'<td class="action">'
-		    			+'<button type="button" class="btn btn-primary btn-xs">Send</button>'
-		    			+'<button type="button" class="btn btn-default btn-xs">Remove</button>'
+		    			+'<button type="button" class="btn btn-primary btn-xs" data-id="'+i+'">Check</button>'
+		    			+'<button type="button" class="btn btn-danger btn-xs" data-id="'+i+'">Remove</button>'
 		    		+'</td>'
-	    			+'</tr>'
+	    			+'</tr>';
 	    	}
+    		$('table.data-table tbody').html(trBody);
+    		for(var i in res){
+    			var tr = $('table.data-table tbody #tr'+i);
+    			var link = $('.link', tr).text();
+    			$('button.btn-primary', tr).on("click", function(){
+    				var options = {
+    					url: link,
+    					type: "GET"
+    				}
+    				alert(link+' send');
+    				ajaxSend(options)
+					.then(function(res){
+
+					})
+					.catch(function(err){
+						console.log(err);
+					})
+    			});
+    			$('button.btn-danger', tr).on("click", function(){
+    				alert(link+' remove');
+    			});
+    		}
 	    }else{
 	    	trBody = '<tr><td colspan="3" class="status">Data is empty!</td></tr>';
+    		$('table.data-table tbody').html(trBody);
 	    }
-    	$('table.data-table tbody').html(trBody);
     }
 	sendResponse({})
 })
+
+function ajaxSend(options){
+	return new Promise(function(resolve, reject){
+		var data = {
+	        url : options["url"],
+	        success: function(res){
+	        	console.log("success send ajax to "+options["url"]);
+	        	resolve(res);
+	        },
+	        error: function (jqXHR, textStatus, errorThrown){
+	            alert('Error adding / update data');
+	        }
+	    };
+	    if(options["type"])
+	    	data.type = options["type"].toUpperCase();
+	    if(options["data"])
+	    	data.data = options["data"];
+	    if(options["dataType"])
+	    	data.dataType = options["dataType"];
+		$.ajax(data);
+	})
+	.catch(function(err){
+		return Promise.reject(err);
+	})
+}
